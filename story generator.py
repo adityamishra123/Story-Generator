f7430d4df10227de297f908ee8fe331881af5083
Aditya Mishra <93374202+adityamishra123@users.noreply.github.com>
2022-03-16 13:16:35 +0530

    Add files via upload

---
+++ story generator.py
@@ -0,0 +1,6 @@
+import random
+when=['A few years ago','yesterday','last night','a long time ago','on 20th january']
+who=['a rabbit','an elephant','a mouse','a turtle','a cat']
+name=['ali','miram','danial','hoouk','starwaker']
+residance=['barcelone','india','germany','venice','england']
+print(random.choice(when)+', '+random.choice(who)+', '+random.choice(name)+', '+random.choice(residance)+', ')
